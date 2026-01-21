e5b3336e5426f609f1bae12c2e1e0ce667ffae29
meghana2 <meghanagurram02102003@gmail.com>
2026-01-21 15:47:38 +0530

    Enhance string and number examples in declaration.js
    Added comments to string method examples for clarity.

--- Day2_stringmethods/index.js
+++ Day2_stringmethods/index.js
@@ -1,18 +1,28 @@
 let name = "MEGHANA"
-console.log(name.length); //7
-console.log(name.toUpperCase()) //MEGHANA
-console.log(name.toLowerCase()) //meghana
-console.log(name[1]) //E
-console.log(name.indexOf("G")) //2
-console.log(name.charAt(4)) //H 
-console.log(name.includes("H")) //true
-console.log(name.startsWith("M")) //true
-console.log(name.endsWith("A")) //true
-console.log(name.concat("GURRAM)) //MEGHANAGURRAM   "))
+console.log(name.length); //7                         //=>length()
+
+console.log(name.toUpperCase()) //MEGHANA             //=>toUpperCase()
+
+console.log(name.toLowerCase()) //meghana             //=>toLowerCase()
+
+console.log(name[1]) //E                              //=>[]
+
+console.log(name.indexOf("G")) //2                   //=>indexOf()
+
+console.log(name.charAt(4)) //H                      //=>charAt()
+
+console.log(name.includes("H")) //true               //=>includes()
+
+console.log(name.startsWith("M")) //true             //=>startsWith()
+
+console.log(name.endsWith("A")) //true               //=>endsWith()
+
+console.log(name.concat("GURRAM")) //MEGHANAGURRAM   //=>concat()  
 
 
 
 let branch = "            CSE              "
-console.log(branch.trimStart()) //"CSE              "
-console.log(branch.trimEnd()) //"            CSE"
-console.log(branch.trim()) //"CSE"
+console.log(branch.trimStart()) //"CSE              "                                  //=>trimStart()
+console.log(branch.trimEnd()) //"            CSE"                                      //=>trimEnd()
+
+console.log(branch.trim()) //"CSE"                                                     //=>trim()
