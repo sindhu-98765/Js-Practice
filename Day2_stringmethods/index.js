c28455f8082fd002dc374d627719b009e2681fc5
meghana2 <meghanagurram02102003@gmail.com>
2026-01-21 15:42:08 +0530

    Add files via upload
    added day2 practice : string methods

---
+++ Day2_stringmethods/index.js
@@ -0,0 +1,18 @@
+let name = "MEGHANA"
+console.log(name.length); //7
+console.log(name.toUpperCase()) //MEGHANA
+console.log(name.toLowerCase()) //meghana
+console.log(name[1]) //E
+console.log(name.indexOf("G")) //2
+console.log(name.charAt(4)) //H 
+console.log(name.includes("H")) //true
+console.log(name.startsWith("M")) //true
+console.log(name.endsWith("A")) //true
+console.log(name.concat("GURRAM)) //MEGHANAGURRAM   "))
+
+
+
+let branch = "            CSE              "
+console.log(branch.trimStart()) //"CSE              "
+console.log(branch.trimEnd()) //"            CSE"
+console.log(branch.trim()) //"CSE"
