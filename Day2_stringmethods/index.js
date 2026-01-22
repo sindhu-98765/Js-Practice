45322348a0a67e33021b38a020d20ef98177e421
meghana2 <meghanagurram02102003@gmail.com>
2026-01-22 20:53:46 +0530

    Refactor string methods examples in index.js

--- Day2_stringmethods/index.js
+++ Day2_stringmethods/index.js
@@ -1,31 +1,20 @@
 let name = "MEGHANA"
 console.log(name.length); //7                         //=>length()
-
 console.log(name.toUpperCase()) //MEGHANA             //=>toUpperCase()
-
 console.log(name.toLowerCase()) //meghana             //=>toLowerCase()
-
 console.log(name[1]) //E                              //=>[]
-
 console.log(name.indexOf("G")) //2                   //=>indexOf()
-
 console.log(name.charAt(4)) //H                      //=>charAt()
-
 console.log(name.includes("H")) //true               //=>includes()
-
 console.log(name.startsWith("M")) //true             //=>startsWith()
-
 console.log(name.endsWith("A")) //true               //=>endsWith()
-
 console.log(name.concat("GURRAM")) //MEGHANAGURRAM   //=>concat()  
 
-//        //
+                                  //        //
 
 let branch = "            CSE              "
 console.log(branch.trimStart()) //"CSE              "                                  //=>trimStart()
-
 console.log(branch.trimEnd()) //"            CSE"                                      //=>trimEnd()
-
 console.log(branch.trim()) //"CSE"                                                     //=>trim()
 
 
@@ -33,9 +22,13 @@ console.log(branch.trim()) //"CSE"                                              
 let name = "Fullstack Masters";
 console.log(name.indexOf(i)); // 2
 console.log(name.lastIndexOf(l)); //3
-
+                                  //                    //
 let sen = "Iam learning Frontend,I want to become a Frontend Developer";
 console.log(sen);
 console.log(sen.replace("Frontend","Fullstack")); //Iam learning Fullstack,I want to become a Frontend Developer
-console.log(sen.replaceAll("Frontend","Fullstack")); ////Iam learning Fullstack,I want to become a Fullstack Developer
+console.log(sen.replaceAll("Frontend","Fullstack")); //Iam learning Fullstack,I want to become a Fullstack Developer
+console.log(sen.slice(0,2)); //IA
+console.log(sen.slice(0, ));//Iam learning Frontend,I want to become a Frontend Developer
+
+
 
