7ae0e7e86c2a536e48765c862901317c98288a8a
meghana2 <meghanagurram02102003@gmail.com>
2026-01-23 18:43:58 +0530

    type coercion,type conversion
    Added examples of type coercion and conversion in JavaScript.

--- Day3_Number_methods/script.js
+++ Day3_Number_methods/script.js
@@ -13,3 +13,23 @@ console.log(typeof c.toString()); //string
 console.log(c.valueOf()); //123.456789
 console.log(parseInt(14.999999)); //14
 console.log(parseFloat(14.999999)); //14.999999
+
+
+// Type Coercion => js converts one data type to another data type automatically
+let d = "15";
+let e = 10;
+console.log(d + e); //1510
+console.log(typeof (d + e)); //string
+console.log(d-e_); //5
+console.log(typeof (d - e)); //number
+console.log(d/e); //1.5
+console.log(typeof (d / e)); //number
+console.log(d*e);//150
+console.log(typeof (d * e)); //number
+
+
+//Type Conversion => we convert one data type to another data type manually
+let x="5"
+let y=10;
+console.log(Number(x)+y); //15
+console.log(String(y)+x); //105
