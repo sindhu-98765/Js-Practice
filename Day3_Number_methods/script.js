73232bdc494d792e0e9a9afe90fc86032225c0a3
meghana2 <meghanagurram02102003@gmail.com>
2026-01-23 18:43:20 +0530

    Add parseFloat example to number methods
    Added parseFloat method demonstration for number conversion.

--- Day3_Number_methods/script.js
+++ Day3_Number_methods/script.js
@@ -11,6 +11,5 @@ console.log(c.toPrecision(4)); //123.5
 console.log(c.toString()); // "123.456789"
 console.log(typeof c.toString()); //string
 console.log(c.valueOf()); //123.456789
-
 console.log(parseInt(14.999999)); //14
 console.log(parseFloat(14.999999)); //14.999999
